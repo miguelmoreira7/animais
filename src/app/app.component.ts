f2fc26775cc512b9615701d6ac7ef6db453dfb13
Miguel Moreira <miguelmoreiracr7@gmail.com>
2023-04-24 19:20:42 -0300

    funções de adicionar form para preencher e adicionar gato feitas. falta um método que garanta que todos os campos foram preenchidos corretamente para submissão de formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Gato} from '../shared/model/gato';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'animais';
-}
+  gatos: Array<Gato>;
+
+  constructor() {
+    this.gatos = new Array<Gato>();
+  }
+  
+  inserirGato(raca: string, idade: number, desc: string): void {
+    const gato = new Gato(raca, idade, desc);
+    this.gatos.push(gato)
+
+  }
+
+  botaoInserir(): void {
+    const dialog = document.createElement('div');
+    dialog.id = 'insertionDialog';
+    dialog.innerHTML = `
+      <form id = "insertion">
+        <label for="racaInput">Raça:</label>
+        <input type="text" id ="racaInput" required>
+        <br>
+        <label for="idadeInput">Idade:</label>
+        <input type="text" id ="idadeInput" required>
+        <br>
+        <label for="desc">Descrição:</label>
+        <input type="text" id ="desc" required>
+      </form>
+
+
+    `;
+    document.body.appendChild(dialog);
+    const button = document.createElement('button');
+    button.id = 'insertButton'
+    button.innerHTML = `Inserir`
+    dialog.appendChild(button);
+    const inserir = document.querySelector("#insertButton")
+    const form = document.querySelector("#insertion") as HTMLFormElement;
+    const raca = form.querySelector("#racaInput") as HTMLInputElement;
+    const idade = form.querySelector("#idadeInput") as HTMLInputElement;
+    const desc = form.querySelector("#desc") as HTMLInputElement;
+    if (raca && idade && desc) {
+      button.addEventListener('click', () => {
+        this.inserirGato(raca.value, parseInt(idade.value), desc.value)
+        document.body.removeChild(dialog);
+      })
+    }
+
+    }
+  }
---
+++ src/shared/model/gato.ts
@@ -0,0 +1,10 @@
+export class Gato {
+    raca: string;
+    idade: number;
+    desc: string;
+    constructor(raca: string, idade: number, desc: string) {
+        this.raca = raca;
+        this.idade = idade;
+        this.desc = desc;
+    }
+}
